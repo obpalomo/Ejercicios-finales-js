41da47532a354b31ea080236ad964aba24eab069
obpalomo <obpalomo@gmail.com>
2023-10-27 20:55:43 +0200

    S1 arrays finalizados

---
+++ S1/3.arrays/exercise-6.js
@@ -0,0 +1,8 @@
+/* Elimina el segundo elemento del array y muestra el array por consola.
+ */
+
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+RickAndMortyCharacters.splice(1,1);
+
+console.log(RickAndMortyCharacters);
