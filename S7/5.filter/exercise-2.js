4517b1dd201ab54f8d3cce8da7f4d6a9b5edaa68
obpalomo <obpalomo@gmail.com>
2023-11-06 20:27:52 +0100

    JS S7 5.filter finalizados

---
+++ S7/5.filter/exercise-2.js
@@ -0,0 +1,11 @@
+/* Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean par. */
+
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const par = ages.filter(age => {
+    if (age % 2 === 0) {
+        return age
+    }
+})
+console.log(par);
